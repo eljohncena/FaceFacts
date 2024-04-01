0c599f3de1dafa229e5fe46fe411c7a420c2fefa
John <john.chavez113@gmail.com>
2024-03-31 20:05:34 -0700

    Added Create and associate locations where you met person

---
+++ FaceFacts/EditEventView.swift
@@ -0,0 +1,24 @@
+//
+//  EditEventView.swift
+//  FaceFacts
+//
+//  Created by John Chavez on 3/31/24.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    @Bindable var event: Event
+    var body: some View {
+        Form {
+            TextField("Name of event", text: $event.name)
+            TextField("Location", text: $event.location)
+        }
+        .navigationTitle("Edit Event")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+//#Preview {
+//    EditEventView()
+//}
--- FaceFacts/EditPersonView.swift
+++ FaceFacts/EditPersonView.swift
@@ -4,12 +4,19 @@
 //
 //  Created by John Chavez on 3/31/24.
 //
-
+import SwiftData
 import SwiftUI
 
 struct EditPersonView: View {
-    
+    @Environment(\.modelContext) var modelContext
     @Bindable var person: Person
+    @Binding var navigationPath: NavigationPath
+    
+    @Query(sort: [
+        SortDescriptor(\Event.name),
+        SortDescriptor(\Event.location)]
+    ) var events: [Event]
+    
     var body: some View {
         Form {
             Section {
@@ -19,13 +26,36 @@ struct EditPersonView: View {
                     .textContentType(.emailAddress)
                     .textInputAutocapitalization(.never)
             }
+            Section("Where did you meet them?") {
+                Picker("Met at", selection: $person.metAt) {
+                    Text("Unknown event")
+                        .tag(Optional<Event>.none)
+                    if events.isEmpty == false {
+                        Divider()
+                        
+                        ForEach(events) { event in
+                            Text(event.name)
+                                .tag(event)
+                        }
+                    }
+                }
+                Button("Add new event", action: addEvent)
+            }
             Section("Notes") {
                 TextField("Details about this person", text: $person.details, axis: .vertical)
             }
         }
         .navigationTitle("Edit Person")
         .navigationBarTitleDisplayMode(.inline)
-        
+        .navigationDestination(for: Event.self) { event in
+                EditEventView(event: event)
+        }
+    }
+    
+    func addEvent() {
+        let event = Event(name: "", location: "")
+        modelContext.insert(event)
+        navigationPath.append(event)
     }
 }
 
