ada91608ef10470d5bdca3dc3b05006500e65e1d
John <john.chavez113@gmail.com>
2024-03-31 21:06:00 -0700

    added photo option for Persons

--- FaceFacts/EditPersonView.swift
+++ FaceFacts/EditPersonView.swift
@@ -4,6 +4,8 @@
 //
 //  Created by John Chavez on 3/31/24.
 //
+
+import PhotosUI
 import SwiftData
 import SwiftUI
 
@@ -11,6 +13,7 @@ struct EditPersonView: View {
     @Environment(\.modelContext) var modelContext
     @Bindable var person: Person
     @Binding var navigationPath: NavigationPath
+    @State private var selectedItem: PhotosPickerItem?
     
     @Query(sort: [
         SortDescriptor(\Event.name),
@@ -19,6 +22,18 @@ struct EditPersonView: View {
     
     var body: some View {
         Form {
+            Section {
+                
+                if let imageData = person.photo, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                }
+                    
+                PhotosPicker(selection: $selectedItem, matching: .images){
+                    Label("Select a photo", systemImage: "person")
+                }
+            }
             Section {
                 TextField("Name", text: $person.name)
                     .textContentType(.name)
@@ -50,12 +65,19 @@ struct EditPersonView: View {
         .navigationDestination(for: Event.self) { event in
                 EditEventView(event: event)
         }
+        .onChange(of: selectedItem, loadPhoto)
     }
     
     func addEvent() {
         let event = Event(name: "", location: "")
         modelContext.insert(event)
         navigationPath.append(event)
+    }
+    
+    func loadPhoto(){
+        Task { @MainActor in
+            person.photo = try await selectedItem?.loadTransferable(type: Data.self)
+        }
     }
 }
 
