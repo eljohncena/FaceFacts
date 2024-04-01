b5546c7fab4b4cf6394010b736712781a6ae34be
John <john.chavez113@gmail.com>
2024-03-31 17:30:36 -0700

    adding new poject

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by John Chavez on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
